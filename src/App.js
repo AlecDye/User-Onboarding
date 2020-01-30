bfea8ad476979e50ce51924ea1a88de7fcf1fc78
Alec Dye <alecdye@gmail.com>
2020-01-30 14:47:43 -0800

    rendering form elements (no functionality)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Form from './Form';
+import UserForm from './Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>App.js - Render Test</h1>
-      <Form />
+      <UserForm />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -3,11 +3,36 @@ import axios from "axios";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const OnboardingForm = (() => {
+
+const UserForm = ((props) => {
     return (
-        <>
-        </>
+        <Form>
+            <label htmlFor="username">
+                <Field id="username" type="text" name="username" />
+            </label>
+            <label htmlFor="email">
+                <Field id="email" type="email" name="email" />
+            </label>
+            <label htmlFor="password">
+                <Field id="password" type="password" name="password" />
+            </label>
+            <label className="checkbox-container" htmlFor="termsOfService">
+                <Field id="termsOfService" type="checkbox" name="termsOfService" />
+            </label>
+            <button type="submit" disabled="isSubmitting">Submit</button>
+        </Form>
     );
 });
 
-export default OnboardingForm;
+const FormikUserForm = withFormik({
+    mapPropsToValues(props) {
+        return {
+            username: props.username || "",
+            email: props.email || "",
+            password: props.password || "",
+            termsOfService: props.termsOfService || false
+        };
+    }
+})(UserForm);
+
+export default FormikUserForm;
