ed3a40032564501699f0a9ceb7c6930bda57bd91
Alec Dye <alecdye@gmail.com>
2020-01-30 13:59:03 -0800

    importing components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from './Form';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>App.js - Render Test</h1>
+      <Form />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -5,7 +5,8 @@ import * as Yup from "yup";
 
 const OnboardingForm = (() => {
     return (
-
+        <>
+        </>
     );
 });
 
